3dd4c99118168413334b52fbd81635ba2b197c2c
Larissa Beato <153762547+LarissaBeato@users.noreply.github.com>
2024-07-31 16:26:26 -0300

    Add files via upload

---
+++ Atvd14/script.js
@@ -0,0 +1,11 @@
+document.write("Base: 2" + "<br>");
+      document.write("Expoente: 8" + "<br><br>");
+      document.write("2<sup>0</sup> = 1" + "<br>");
+      document.write("2<sup>1</sup> = " + (2*1) + "<br>");
+      document.write("2<sup>2</sup> = " + (2*2) + "<br>");
+      document.write("2<sup>3</sup> = " + (2*2*2) + "<br>");
+      document.write("2<sup>4</sup> = " + (2*2*2*2) + "<br>");
+      document.write("2<sup>5</sup> = " + (2*2*2*2*2) + "<br>");
+      document.write("2<sup>6</sup> = " + (2*2*2*2*2*2) + "<br>");
+      document.write("2<sup>7</sup> = " + (2*2*2*2*2*2*2) + "<br>");
+      document.write("2<sup>8</sup> = " + (2*2*2*2*2*2*2*2) + "<br>");
